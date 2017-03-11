4596c671d2dc72c61ef363e39ef9d2f7435dea0a
RubenTipparach <santipab.stephane@gmail.com>
2017-03-11 02:04:32 -0600

    Adding socket io module, comminicated with unity server from infinite-synaptic.

---
+++ Temporary_Android_Version/FleetHackersTaccom-Android/Assets/Scripts/Networking/SocketRTS.cs
@@ -0,0 +1,27 @@
+﻿using SocketIO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocketRTS : MonoBehaviour {
+
+
+    [SerializeField]
+    SocketIOComponent socket;
+
+    // Use this for initialization
+    void Start () {
+        socket.On("hello", OnConnect);
+        socket.Emit("response");
+    }
+	
+    void OnConnect(SocketIOEvent e)
+    {
+        Debug.Log("client connected to server.");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
