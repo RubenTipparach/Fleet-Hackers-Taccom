f2e1da631bea6697a208fc1d423839c0082001e2
RubenTipparach <santipab.stephane@gmail.com>
2017-03-12 05:43:50 -0500

    Tested advanced networking protocol.

--- Temporary_Android_Version/FleetHackersTaccom-Android/Assets/Scripts/Networking/SocketRTS.cs
+++ Temporary_Android_Version/FleetHackersTaccom-Android/Assets/Scripts/Networking/SocketRTS.cs
@@ -1,4 +1,5 @@
 ﻿using SocketIO;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,19 +10,58 @@ public class SocketRTS : MonoBehaviour {
     [SerializeField]
     SocketIOComponent socket;
 
+
     // Use this for initialization
     void Start () {
-        socket.On("hello", OnConnect);
-        socket.Emit("response");
+        socket.On("intialize", OnConnect);
+
+        socket.On("spawn-ship", SpawnShip);
+
+        StartCoroutine(DelayedEmit());
     }
-	
-    void OnConnect(SocketIOEvent e)
+
+    private void SpawnShip(SocketIOEvent se)
+    {
+        //obj.data
+    }
+
+    void OnConnect(SocketIOEvent se)
     {
         Debug.Log("client connected to server.");
     }
 
-	// Update is called once per frame
-	void Update () {
-		
+
+    public IEnumerator DelayedEmit()
+    {
+        yield return new WaitForSeconds(1);
+
+        socket.Emit("register-unity-server",
+            SIMessage.ToJSO(
+                new RTSServer { serverId = "rubens-srever" }));
+    }
+
+    // Update is called once per frame
+    void Update () {
+
 	}
 }
+
+public static class SIMessage
+{
+    public static JSONObject ToJSO<T>(T input)
+    {
+        return JSONObject.Create(JsonUtility.ToJson(input));
+    }
+
+    public static T FromJSO<T>(string json)
+    {
+        return JsonUtility.FromJson<T>(json);
+    }
+}
+
+[Serializable]
+public class RTSServer
+{
+    public string serverId;
+}
+
