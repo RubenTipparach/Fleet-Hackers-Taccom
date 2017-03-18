2c3f4e619d320305435440085c7975d8a9131e91
RubenTipparach <santipab.stephane@gmail.com>
2017-03-18 16:49:27 -0500

    Outlined position transfer protocol.

---
+++ Temporary_Android_Version/FleetHackersTaccom-Android/Assets/Scripts/Networking/BasicNetworkShip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+using System;
+
+public class BasicNetworkShip : MonoBehaviour {
+
+
+    [SerializeField]
+    public SocketIOComponent socket;
+
+    public string shipId;
+    // Use this for initialization
+    void Start () {
+		socket.Emit("move-ship", SIMessage.ToJSO(new NetworkShipPosition()));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
+[Serializable]
+public class NetworkShipPosition
+{
+    public string shipId;
+}
